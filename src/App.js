8595b13d79719871e156212d001991265daf8c22
leadnatenitride <107972483+leadnateniride@users.noreply.github.com>
2022-10-02 16:41:09 -0700

    a cat app with MUI library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 function App() {
+  const defaulttext = "A Random Cat Fact";
+  const [text, setText] = useState(defaulttext);
+
+  function query() {
+    return fetch('https://catfact.ninja/fact')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      const text = responseJson.fact;
+      setText(text);
+    })
+    .catch((error) => console.error(error));
+  }
+
+  function BtnClick(){
+    query()
+  }
+
+  function QueryBtn(){
+    return (
+      <Button variant="outlined" onClick={BtnClick}>
+          {/* Click 🐱 */}
+          🐱
+      </Button>
+    )
+  }
+
+  function QueryText(){
+    return (
+      <Box sx={{
+        width: 600,
+        height: 300,
+      }}
+      >
+        <p>{text}</p>
+      </Box>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QueryText/>
+        <QueryBtn/>
       </header>
     </div>
   );
